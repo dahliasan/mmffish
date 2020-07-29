c0e6fbb51aa60c5f4e95b6b8a57a3288e7239074
oofd <dahlia.foo@utas.edu.au>
2020-07-29 19:59:11 +1000

    init
    init

---
+++ data/LIST_MARINE_LEASES_STATEWIDE/readme.txt
@@ -0,0 +1,51 @@
+Extraction Date: Wednesday 18 September 2019
+
+Dataset: list_marine_leases_statewide
+
+	All data is supplied in GDA94 / Map Grid of Australia Zone 55
+
+Selected Area: Statewide Coverage
+
+Files Supplied:
+	list_marine_leases_statewide.gdb
+	list_marine_leases_statewide.cpg
+	list_marine_leases_statewide.DAT
+	list_marine_leases_statewide.dbf
+	list_marine_leases_statewide.ID
+	list_marine_leases_statewide.MAP
+	list_marine_leases_statewide.prj
+	list_marine_leases_statewide.sbn
+	list_marine_leases_statewide.sbx
+	list_marine_leases_statewide.shp
+	list_marine_leases_statewide.shp.xml
+	list_marine_leases_statewide.shx
+	list_marine_leases_statewide.tab
+	readme.txt
+
+Licensing:
+	All Data is Copyright © State of Tasmania. Data is supplied under
+	Creative Commons Attribution 3.0 Australia (CC BY 3.0 AU)
+	http://creativecommons.org/licenses/by/3.0/au/
+	For appropriate attribution see the Land Tasmania Attribution Guidelines at
+	http://listdata.thelist.tas.gov.au/public/LandTasmaniaDataAttributionGuidelines.pdf
+
+Disclaimer:
+	The Crown in the Right of Tasmania and its employees and agents: give
+	no warranty regarding the Data's accuracy, completeness, currency or
+	suitability for any particular purpose and those using it for whatever
+	purpose are advised to verify it with the relevant Commonwealth or State
+	government department, local government body or other source and to
+	obtain any appropriate professional advice; and do not accept liability
+	however arising including, but not limited to negligence for any loss
+	resulting from the use of or reliance upon the Data or reliance on its
+	availability at any time; give no warranty that the Data is free of
+	infection by computer viruses or other contamination, nor that access to
+	http://listdata.thelist.tas.gov.au/opendata or any part of it will not
+	suffer interruption from time to time, without notice. Data structures
+	may vary between format types and delivery period.
+
+For more information contact:
+	Land Tasmania Client Services
+	Email: geodata.clientservices@dpipwe.tas.gov.au
+	Phone: (03)6165 4444
+	Web: www.thelist.tas.gov.au
---
+++ map.R
@@ -0,0 +1,127 @@
+# Plot map of marine finfish farms and seal haulout + breeding locations
+
+library(sf)
+library(tidyverse)
+library(tmap)
+library(tmaptools)
+library("rnaturalearth")
+library("rnaturalearthdata")
+library(raster)
+
+# Load shape file
+dir("data")
+farm <- st_read("data/LIST_MARINE_LEASES_STATEWIDE/list_marine_leases_statewide.shp")
+
+# Load finfish farm lease number reference
+finfarm_id <- read_csv("data/finfish_licences.csv")
+
+finfarm <- farm %>% 
+  filter(M_LEASE_NO %in% finfarm_id$lease_no)
+
+# Load seal locations
+breed <- st_read("data/breeding_sites.kml")
+haulout <- st_read("data/haulouts.kml")
+
+
+# Plot --------------------------------------------------------------------
+
+## Convert all datasets to same crs
+world <- ne_countries(scale = "large", returnclass = "sf")
+finfarm <- st_transform(finfarm, crs = crs(world, asText = TRUE))
+breed <- st_transform(breed, crs = crs(world, asText = TRUE))
+haulout <- st_transform(haulout, crs = crs(world, asText = TRUE))
+
+
+# Combine all locations to one dataframe
+finfarm <- finfarm %>% mutate(loc_type = "Finfish Farm")
+breed <- breed %>% mutate(loc_type = "Breeding Site")
+haulout <- haulout %>% mutate(loc_type = "Haul-out")
+
+loc <- rbind(breed, haulout) %>% 
+  dplyr::select(-Description)
+
+finfarm_tmp <- finfarm %>%
+  rename(Name = M_LEASE_NO) %>%
+  dplyr::select(Name, loc_type) %>% 
+  st_cast("MULTIPOINT")
+
+loc <- rbind(loc, finfarm_tmp) %>% 
+  mutate(loc_type = factor(loc_type, levels = c('Finfish Farm', 'Breeding Site', 'Haul-out')))
+
+## Set colour palette
+pal1 <- get_brewer_pal("Dark2", n = 2)
+
+
+## Create bounding box for tasmania
+tas_bbox <- st_bbox(c(xmin = 143.4, xmax = 148.8, ymax = -39.2, ymin = -43.9), crs = st_crs(world))
+## Tasmania base map
+tasmap1 <- tm_shape(world, bbox = tas_bbox) +
+  tm_fill() +
+  tm_borders() 
+  
+## Add finfish farm locations
+tasmap1 +
+  tm_shape(finfarm) +
+  tm_dots(col = 'red', size = 0.2) + 
+  # Add seal haulouts
+  tm_shape(haulout) + 
+  tm_dots(col = 'blue', size = 0.2) 
+
+
+
+## Inset map to SE Tas
+setas_bbox <- st_bbox(c(xmin = 146.8, xmax = 148.3, ymax = -42.2, ymin = -43.9), crs = st_crs(world))
+
+tm_shape(world, bbox = setas_bbox) +
+  tm_polygons() + 
+  tm_shape(finfarm) +
+  # tm_polygons(col = 'red') + 
+  tm_dots(col = 'red', size = 0.2) +
+  # Add seal haulouts
+  tm_shape(haulout) + 
+  tm_dots(col = 'blue', size = 0.2)
+
+## Create Tas map with SE Tas region box
+
+# Create SE Tas region box
+setas_region <- setas_bbox %>% st_as_sfc()
+
+p1 <- tasmap1 +
+  # Add all location types
+  tm_shape(loc) +
+  tm_symbols(col = "loc_type", shape = "loc_type", size = 0.2, palette = c(pal1[1], pal1[2], pal1[2]), shapes = c(21,3,21), legend.shape.show = FALSE, shapes.legend = c(21,3,21), title.col = '') + 
+  ## Add SE Tas region box
+  # tm_shape(setas_region) + tm_borders(lwd = 3) + 
+  # Add map features
+  tm_grid(lines = FALSE, labels.size = 0.8) +
+  tm_compass(position = c(.03, .25), size  = 1) +
+  tm_scale_bar(breaks = c(0, 50, 100), position = c(0.03, .15), text.size = 1) + 
+  tm_style("col_blind", legend.text.size = 1) 
+
+tmap_save(p1, filename = 'Finfish x Seal Map.png', units = 'in', height = 7, width = 6)
+
+
+ggplot(farm_lease_fin) + 
+  geom_sf() + 
+  theme_bw()
+  
+
+au_map <- world[world$iso_a2 == "AU",]
+plot(st_geometry(au_map))
+
+
+tm_shape(farm_lease_fin) + 
+  tm_polygons(palette = "Greens") + 
+  tm_shape(au_map) + 
+  tm_polygons(fill = 'red')
+
+
+library(sf)
+library(raster)
+library(dplyr)
+library(spData)
+library(spDataLarge)
+library(tmap)    # for static and interactive maps
+library(leaflet) # for interactive maps
+library(ggplot2) # tidyverse data visualization package
+
